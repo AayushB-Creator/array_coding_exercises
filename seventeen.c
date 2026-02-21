672615728f53dacaf68f60677c9275e0b5a6fb31
Aayush B <aayushb22.cse@gmail.com>
2026-02-21 09:55:44 +0530

    seventeen.c

---
+++ seventeen.c
@@ -0,0 +1,22 @@
+/*
+wap that reads a matrix and displays 
+the sum of elems below the main diagonal
+*/
+#include <stdio.h>
+int main(){
+    int mat[5][5];
+    printf("Enter the elements of 5x5 matrix \n");
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    int sum=0;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<i;j++){
+            if(i>j) sum+=mat[i][j];
+        }
+    }
+    printf("The sum of elems below main diagonal is : %d",sum);
+    return 0;
+}
