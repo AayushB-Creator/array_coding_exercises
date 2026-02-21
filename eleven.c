67b83fa00594e75d7a38f58914d6b0f3519b308b
Aayush B <aayushb22.cse@gmail.com>
2026-02-21 07:43:51 +0530

    eleven.c

---
+++ eleven.c
@@ -0,0 +1,11 @@
+/*
+wap to i/p the elements of a 2D array
+then from this array, make two arrays -
+one that stores all odd elements of the 2D array
+and another one that stores all even elements of the 2D array
+*/
+#include <stdio.h>
+int main(){
+    int arr[]
+    return 0;
+}
